9d89d69f5003c476a69aada621a46535062f8547
leon.bergerhoff <leon.bergerhoff@mpi-dortmund.mpg.de>
2023-05-26 12:32:06 +0200

    added the messagehandler Echo_All

--- main.py
+++ main.py
@@ -1,53 +1,15 @@
-from matplotlib import pyplot as plt
+import os
+import telebot
 
-integer = 1
-floating_point = 1.0
-string = "string"
-liste = [1, 2, 3.0, "hallo"]
-dictionary = {"key": "value", "key2": 1}
+BOT_TOKEN = os.environ.get('BOT_TOKEN')
+bot = telebot.TeleBot(BOT_TOKEN)
 
-def pqformel(a, b, c):
-    p = b / a
-    q = c / a
-    if q > 0:
-        raise ValueError("Q cannot be greater than 0")
-    t1 = -p/2
-    t2 = ((p/2)**2-q)**(1/2)
-    return t1 + t2, t1 - t2
+@bot.message_handler(commands=['start' , 'hello'])
+def send_welcome(message):
+    bot.reply_to(message, "Hey, How is going?")
 
-loaded_data = []
-with open("daten.csv") as file:
-    for line in file.readlines():
-        if not line.startswith("#") and line.strip():
-            entry_list = []
-            for entry in line.strip().split(","):
-                entry_list.append(float(entry))
-            loaded_data.append(entry_list)
+@bot.message_handler(func=lambda msg: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
 
-print(loaded_data)
-
-
-def quadrat(a, b, c, x):
-    return a*x**2+b*x+c
-
-
-for a, b, c in loaded_data:
-    try:
-        x1, x2 = pqformel(a, b, c)
-    except ValueError as e:
-        x1, x2 = [None, None]
-
-    x = range(-10, 10)
-    y = []
-    for entry_x in x:
-        y.append(quadrat(a, b, c, entry_x))
-
-    if x1 is not None:
-        plt.axvline(x1, color='orange')
-    if x2 is not None:
-        plt.axvline(x2, color='orange')
-    plt.plot(x, y)
-    plt.title(f"{a=} {b=} {c=}")
-    plt.grid()
-    plt.show()
-    plt.clf()
+bot.infinity_polling()
